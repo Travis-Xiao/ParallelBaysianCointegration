98a173fa26b272ece375ea18e7275406b75b3dc7
Travis-Xiao <Travis.Xiao@gmail.com>
2015-09-11 14:43:00 +0800

    'first'

---
+++ start-para.bat
@@ -0,0 +1,7 @@
+java -jar PBIShift-Parallel.jar Coffee ./UCR_TS_Archive_2015/ 22 0 14
+java -jar PBIShift-Parallel.jar Coffee ./UCR_TS_Archive_2015/ 22 14 28
+java -jar PBIShift-Parallel.jar Coffee ./UCR_TS_Archive_2015/ 22 28 42
+java -jar PBIShift-Parallel.jar Coffee ./UCR_TS_Archive_2015/ 22 42 56
+java -jar PBIShift-Parallel.jar OSULeaf ./UCR_TS_Archive_2015/ 22 0 150
+java -jar PBIShift-Parallel.jar OSULeaf ./UCR_TS_Archive_2015/ 22 150 300
+java -jar PBIShift-Parallel.jar OSULeaf ./UCR_TS_Archive_2015/ 22 300 442
